3d4c5b61d895ca2f7a5acd4237049bc18d4dae85
Zachary Sailer <zachsailer@gmail.com>
2016-09-06 16:48:28 -0700

    napoleon configs

--- docs/conf.py
+++ docs/conf.py
@@ -304,3 +304,15 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+# Napoleon settings
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = True
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
