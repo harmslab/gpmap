b0525ab4ea54cfd72d0edb2d155a7c11adabf51e
Zachary Sailer <zachsailer@gmail.com>
2017-10-11 10:59:04 -0700

    two args returned by mask

--- gpmap/simulate/mask.py
+++ gpmap/simulate/mask.py
@@ -5,12 +5,24 @@ from .. import GenotypePhenotypeMap
 def mask(gpm, mask_fraction):
     """Create a new GenotypePhenotypeMap object from a random subset of another
     GenotypePhenotypeMap. 
+    
+    
+    Returns 
+    -------
+    true_mask_fraction : float
+        the actual fraction used, since the space is discrete and likely won't be
+        the exact fraction given.
+    GenotypePhenotypeMap : 
+        the new genotype-phenotype map.
     """
     if mask_fraction > 1 or mask_fraction < 0:
         raise Exception("mask_fraction must between between 0 and 1.")
     
     # Calculate the number of genotypes to select
     number_to_choose = int((1-mask_fraction) * gpm.n)
+    
+    # Calculate the true fraction (since this is a discrete space.)
+    true_mask_fraction = float(number_to_choose)/gpm.n
     
     # Randomly choose genotypes
     index = np.random.choice(gpm.index, number_to_choose, replace=False)
@@ -24,11 +36,12 @@ def mask(gpm, mask_fraction):
         raise Exception("n_replicates are not a valid dtype.")
         
     # return Subset genotype
-    return GenotypePhenotypeMap(
-        gpm.wildtype,
-        gpm.genotypes[index].reset_index(drop=True),
-        gpm.phenotypes[index].reset_index(drop=True),
-        mutations=gpm.mutations,
-        stdeviations=gpm.stdeviations[index].reset_index(drop=True),
-        n_replicates=n_replicates)
+    return true_mask_fraction,
+        GenotypePhenotypeMap(
+            gpm.wildtype,
+            gpm.genotypes[index].reset_index(drop=True),
+            gpm.phenotypes[index].reset_index(drop=True),
+            mutations=gpm.mutations,
+            stdeviations=gpm.stdeviations[index].reset_index(drop=True),
+            n_replicates=n_replicates)
     
