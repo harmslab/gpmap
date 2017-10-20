50a82d293fa754a9131dd5b4429cda0fb307a191
Zachary Sailer <zachsailer@gmail.com>
2017-10-20 14:57:35 -0700

    add random simulation

---
+++ gpmap/simulate/random.py
@@ -0,0 +1,16 @@
+import numpy as np
+from .base import BaseSimulation
+
+class RandomPhenotypesSimulation(BaseSimulation):
+    """ Build a simulated GenotypePhenotypeMap. Generates random phenotypes.
+    """
+    def __init__(self, wildtype, mutations, phenotype_range=(0,1), *args, **kwargs):
+        # build genotypes
+        super(RandomPhenotypesSimulation, self).__init__(wildtype, mutations, *args, **kwargs)  
+        self.phenotype_range = phenotype_range
+        self.build()
+
+    def build(self):
+        """Build phenotypes"""
+        low, high = self.phenotype_range[0], self.phenotype_range[1]
+        self.phenotypes = np.random.uniform(low, high, size=self.genotypes)
