32720c3632125362e175fcd01ccff5f8eb33a215
Zsailer <zachsailer@gmail.com>
2018-05-10 14:17:00 -0700

    new setup.py and version

--- gpmap/__init__.py
+++ gpmap/__init__.py
@@ -3,3 +3,5 @@ __version__ = '0.1'
 
 # Import the main module in this package
 from gpmap.gpm import GenotypePhenotypeMap
+
+from .__version__ import __version__
--- setup.py
+++ setup.py
@@ -1,33 +1,117 @@
-try:
-    from setuptools import setup
-except:
-    from distutils.core import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# Packages and subpackages to install
-packages = [
-    'gpmap',
-    'gpmap.simulate'
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'gpmap'
+DESCRIPTION = 'A Python API for managing genotype-phenotype map data'
+URL = 'https://github.com/harmslab/gpmap'
+EMAIL = 'zachsailer@gmail.com'
+AUTHOR = 'Zachary R. Sailer'
+REQUIRES_PYTHON = '>=3.3.0'
+VERSION = None
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    "numpy",
+    "scipy",
+    "pandas"
 ]
 
-setup(name='gpmap',
-      version='0.4.1',
-      description='Data-structure for analyzing genotype-phenotype map data.',
-      author='Zach Sailer',
-      author_email='zachsailer@gmail.com',
-      packages=packages,
-      url="https://github.com/harmslab/gpmap",
-      install_requires=[
-          'numpy',
-          'scipy',
-          'pandas'
-      ],
-      classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: Science/Research',
-            'Topic :: Scientific/Engineering :: Bio-Informatics',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.2',
-            'Programming Language :: Python :: 3.3',
-            'Programming Language :: Python :: 3.4',
-        ],
-      zip_safe=False)
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
