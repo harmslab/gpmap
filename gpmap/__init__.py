a6e5e371e807d8ce1043c02ab81c49ebe0faa596
Luis Perez <lperezmo@uoregon.edu>
2020-06-29 22:07:34 -0700

    Unit tests and example notebook
    Added unit tests for GenotypePhenotypeMap object and updated the example notebook in the 'Examples' folder.

--- gpmap/__init__.py
+++ gpmap/__init__.py
@@ -2,6 +2,6 @@ __doc__ = """"""
 __version__ = '0.1'
 
 # Import the main module in this package
-from gpmap.gpm import GenotypePhenotypeMap
+import gpmap.gpm
 
 from .__version__ import __version__
--- gpmap/tests/test_gpm.py
+++ gpmap/tests/test_gpm.py
@@ -1,81 +1,97 @@
-# import numpy as np
-# import pytest
-#
-# from ..gpm import GenotypePhenotypeMap
-#
-# @pytest.fixture()
-# def tmp_gpm_file():
-#     pass
-#
-#
-# class testGenotypePhenotypeMap:
-#
-#     def _test_attributes(self, gpm):
-#         # Test instance was created
-#         assert isinstance(gpm, GenotypePhenotypeMap)
-#         # Test elements align
-#         np.testing.assert_array_equal(gpm.genotypes, self.genotypes)
-#
-#     def test_init(self):
-#         """Test initialization of class"""
-#         gpm = GenotypePhenotypeMap(
-#             self.wildtype,
-#             self.genotypes,
-#             self.phenotypes,
-#             n_replicates=self.n_replicates,
-#             mutations=self.mutations
-#         )
-#         # Test instance was created
-#         tools.assert_is_instance(gpm, GenotypePhenotypeMap)
-#         # Test elements align
-#         np.testing.assert_array_equal(gpm.genotypes, self.genotypes)
-#         np.testing.assert_array_equal(gpm.phenotypes, self.phenotypes)
-#
-#     def test_read_json(self):
-#         """Test reading from json"""
-#         # gpm = GenotypePhenotypeMap.read_json("data.json")
-#         # Test instance was created
-#         tools.assert_is_instance(gpm, GenotypePhenotypeMap)
-#         # Test elements align
-#         np.testing.assert_array_equal(gpm.genotypes, self.genotypes)
-#
-#     def test_length(self):
-#         """Test genotype length."""
-#         gpm = GenotypePhenotypeMap.read_json("data.json")
-#         tools.assert_equal(self.GPM.length, 4)
-#
-#     def test_n(self):
-#         """Test size"""
-#         tools.assert_equal(self.GPM.n, 2**4)
-#
-#     def test_std(self):
-#         """Test raw errors"""
-#         np.testing.assert_array_equal(self.GPM.std.upper, self.stdeviations)
-#
-#     def test_mutant(self):
-#         """Test mutant"""
-#         tools.assert_equal(self.GPM.mutant, "1111")
-#
-#     def test_missing_genotypes(self):
-#         """Test that missing genotypes are identified."""
-#         # Choose a subset indices for testing missing genotypes.
-#         index = np.arange(0,16)
-#         np.random.shuffle(index)
-#         # Get subset genotypes
-#         chosen_g = np.array(self.genotypes)[index[:8]]
-#         missing_g = np.array(self.genotypes)[index[8:]]
-#
-#         # Get subset phenotypes
-#         chosen_p = np.array(self.phenotypes)[index[:8]]
-#         # Init GPM
-#         gpm = GenotypePhenotypeMap(
-#             self.wildtype,
-#             chosen_g,
-#             chosen_p,
-#             log_transform=self.log_transform,
-#             n_replicates=self.n_replicates,
-#             mutations=self.mutations,
-#         )
-#         # Test missing genotypes are returned
-#         np.testing.assert_array_equal(gpm.genotypes, chosen_g)
-#         np.testing.assert_array_equal(np.sort(gpm.missing_genotypes), np.sort(missing_g))
+import numpy as np
+import pytest
+from IPython.testing import tools
+
+from gpmap import utils
+from ..gpm import GenotypePhenotypeMap
+
+
+@pytest.fixture()
+def tmp_gpm_file():
+    wildtype = "AAA"
+
+    genotypes = [
+        "AAA",
+        "AAB",
+        "ABA",
+        "BAA",
+        "ABB",
+        "BAB",
+        "BBA",
+        "BBB"
+    ]
+
+    binary = [
+        '000',
+        '001',
+        '010',
+        '100',
+        '011',
+        '101',
+        '110',
+        '111'
+    ]
+
+    mutations = {
+        0: ["A", "B"],
+        1: ["A", "B"],
+        2: ["A", "B"],
+    }
+    phenotypes = np.random.rand(len(genotypes))
+    tmp_gpm_file = GenotypePhenotypeMap(wildtype=wildtype,
+                                        genotypes=genotypes,
+                                        phenotypes=phenotypes,
+                                        log_transform=False,
+                                        mutations=mutations)
+    return tmp_gpm_file
+
+
+def test_read_json(tmp_gpm_file):
+    """Test reading from json"""
+    read_gpm = GenotypePhenotypeMap.read_json("gpmap/tests/data/test_data.json")
+    # Test instance was created
+    assert isinstance(read_gpm, GenotypePhenotypeMap)
+    # Test elements align
+    np.testing.assert_array_equal(tmp_gpm_file.genotypes, read_gpm.genotypes)
+
+def test_length(tmp_gpm_file):
+    """Test genotype length."""
+    assert tmp_gpm_file.length == 3
+
+
+def test_n(tmp_gpm_file):
+    """Test size"""
+    assert tmp_gpm_file.n == 8
+
+
+def test_std(tmp_gpm_file):
+    """Test raw errors"""
+    np.testing.assert_array_equal(tmp_gpm_file.std.upper, tmp_gpm_file.stdeviations)
+
+
+def test_mutant(tmp_gpm_file):
+    """Test mutant"""
+    assert tmp_gpm_file.mutant == "BBB"
+
+
+def test_missing_genotypes(tmp_gpm_file):
+    """Test that missing genotypes are identified."""
+    # Choose a subset indices for testing missing genotypes.
+    index = np.arange(0, 8)
+    np.random.shuffle(index)
+    # Get subset genotypes
+    chosen_g = np.array(tmp_gpm_file.genotypes)[index[:4]]
+    missing_g = np.array(tmp_gpm_file.genotypes)[index[4:]]
+
+    # Get subset phenotypes
+    chosen_p = np.array(tmp_gpm_file.phenotypes)[index[:4]]
+
+    gpm_missing_g = GenotypePhenotypeMap(wildtype=tmp_gpm_file.wildtype,
+                                         genotypes=chosen_g,
+                                         phenotypes=chosen_p,
+                                         log_transform=False,
+                                         mutations=tmp_gpm_file.mutations
+                                         )
+
+    np.testing.assert_array_equal(gpm_missing_g.genotypes, chosen_g)
+    np.testing.assert_array_equal(np.sort(gpm_missing_g.get_missing_genotypes()), np.sort(missing_g))
