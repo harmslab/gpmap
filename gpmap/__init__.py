bd5625e85de34eb0a7b7e26826e6a6f0ca2177be
Luis Perez Morales <61613934+lperezmo@users.noreply.github.com>
2020-07-01 12:27:59 -0700

    Update __init__.py

--- gpmap/__init__.py
+++ gpmap/__init__.py
@@ -2,6 +2,6 @@ __doc__ = """"""
 __version__ = '0.1'
 
 # Import the main module in this package
-import gpmap.gpm
+from gpmap.gpm import GenotypePhenotypeMap
 
 from .__version__ import __version__
