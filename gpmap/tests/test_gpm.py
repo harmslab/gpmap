56846687788e0e0fad9c19823fd00bd8c0648e33
Luis Perez <lperezmo@uoregon.edu>
2020-08-21 16:33:13 -0700

    Reformatted unit tests so Travis CI can understand them
    Changed location of file names, and replaced a dynamic unit test with a fixed unit test to prevent Travis CI from hating us

--- gpmap/tests/test_gpm.py
+++ gpmap/tests/test_gpm.py
@@ -46,9 +46,50 @@ def tmp_gpm_file():
     return tmp_gpm_file
 
 
+@pytest.fixture()
+def fixed_gpm_object():
+    wildtype = "AAA"
+
+    genotypes = [
+        "AAA",
+        "AAB",
+        "ABA",
+        "BAA",
+        "ABB",
+        "BAB",
+        "BBA",
+        "BBB"
+    ]
+
+    binary = [
+        '000',
+        '001',
+        '010',
+        '100',
+        '011',
+        '101',
+        '110',
+        '111'
+    ]
+
+    mutations = {
+        0: ["A", "B"],
+        1: ["A", "B"],
+        2: ["A", "B"],
+    }
+    phenotypes = np.array([0.62589116, 0.18940848, 0.36200286, 0.38687046, 0.92057246, 0.41535576, 0.58436333,
+                           0.03187195])
+    fixed_gpm_object = GenotypePhenotypeMap(wildtype=wildtype,
+                                            genotypes=genotypes,
+                                            phenotypes=phenotypes,
+                                            log_transform=False,
+                                            mutations=mutations)
+    return fixed_gpm_object
+
+
 def test_read_json(tmp_gpm_file):
     """Test reading from json"""
-    read_gpm = GenotypePhenotypeMap.read_json("data/test_data.json")
+    read_gpm = GenotypePhenotypeMap.read_json("gpmap/tests/data/test_data.json")
     # Test instance was created
     assert isinstance(read_gpm, GenotypePhenotypeMap)
     # Test elements align
@@ -57,22 +98,18 @@ def test_read_json(tmp_gpm_file):
 
 def test_read_csv():
     """Test reading from csv"""
-    read_gpm = GenotypePhenotypeMap.read_csv(fname="data/test_data.csv", wildtype='AAA')
+    read_gpm = GenotypePhenotypeMap.read_csv(fname="gpmap/tests/data/test_data.csv", wildtype='AAA')
     # Test instance was created
     assert isinstance(read_gpm, GenotypePhenotypeMap)
 
 
-def test_data_integrity_csv(tmp_gpm_file):
-    # Export tmp_gpm_file to csv, give it time to generate file, and then check integrity
-    # of the data read by "read_csv". This is necessary because phenotypes are randomly generated.
-    tmp_gpm_file.to_csv(filename="data/test_data.csv")
-    time.sleep(1)
-    read_gpm = GenotypePhenotypeMap.read_csv(fname="data/test_data.csv", wildtype='AAA')
-    # Test elements align
-    np.testing.assert_array_equal(tmp_gpm_file.genotypes, read_gpm.genotypes)
-    np.testing.assert_array_equal(tmp_gpm_file.phenotypes, read_gpm.phenotypes)
-    np.testing.assert_array_equal(tmp_gpm_file.mutations, read_gpm.mutations)
-    np.testing.assert_array_equal(tmp_gpm_file.binary, read_gpm.data.binary)
+def test_data_integrity_csv(fixed_gpm_object):
+    read_gpm = GenotypePhenotypeMap.read_csv(fname="gpmap/tests/data/test_data.csv", wildtype='AAA')
+    # Test data inside of fixed object is the same
+    np.testing.assert_array_equal(fixed_gpm_object.genotypes, read_gpm.genotypes)
+    np.testing.assert_array_equal(fixed_gpm_object.phenotypes, read_gpm.phenotypes)
+    np.testing.assert_array_equal(fixed_gpm_object.mutations, read_gpm.mutations)
+    np.testing.assert_array_equal(fixed_gpm_object.binary, read_gpm.binary)
 
 
 def test_length(tmp_gpm_file):
